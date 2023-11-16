4f6ca5a1025f170de1612ec601680e87352efd26
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-16 09:21:30 -0300

    Initial Commit

---
+++ barcodescanner/barcodescannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcodescannerApp.swift
+//  barcodescanner
+//
+//  Created by Agustin Carbajal on 16/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct barcodescannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
